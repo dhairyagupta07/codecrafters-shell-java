42eceba1eebca80bf00b28fdbeb697adee039955
Dhairya Gupta <dhairya71106@gmail.com>
2026-06-19 19:35:14 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
-        System.out.print("$ ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("$ ");
+            System.out.flush();
+
+            String input = sc.nextLine();
+
+            // exit stage (usually already required)
+            if (input.equals("exit 0")) {
+                break;
+            }
+
+            // invalid command handling (this is #CZ2)
+            System.out.println(input + ": command not found");
+        }
+
+        sc.close();
     }
 }
