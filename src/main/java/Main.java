43a3da694c4209a361019d9091fe6b29a00c65c1
Dhairya Gupta <dhairya71106@gmail.com>
2026-06-19 19:42:47 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,7 @@
-import java.io.File;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
@@ -15,45 +14,29 @@ public class Main {
                 break;
             }
 
-            // echo
             if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
             }
-
-            // type command
             else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
 
-                // builtins
                 if (cmd.equals("echo") ||
                     cmd.equals("exit") ||
                     cmd.equals("type")) {
                     System.out.println(cmd + " is a shell builtin");
-                    continue;
-                }
-
-                // search PATH
-                String pathEnv = System.getenv("PATH");
-                String[] paths = pathEnv.split(":");
-                boolean found = false;
-
-                for (String path : paths) {
-                    File file = new File(path, cmd);
-                    if (file.exists() && file.canExecute()) {
-                        System.out.println(cmd + " is " + file.getAbsolutePath());
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
+                } else {
                     System.out.println(cmd + ": not found");
                 }
             }
 
-            // unknown command
             else {
-                System.out.println(input + ": command not found");
+                String[] cmd = input.split(" ");
+
+                ProcessBuilder pb = new ProcessBuilder(cmd);
+                pb.inheritIO();
+
+                Process process = pb.start();
+                process.waitFor();
             }
         }
 
