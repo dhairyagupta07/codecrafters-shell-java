8e7ac0fdf931fb254b96979b42239174ee7be7ab
Dhairya Gupta <dhairya71106@gmail.com>
2026-06-19 19:41:21 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -14,22 +15,43 @@ public class Main {
                 break;
             }
 
+            // echo
             if (input.startsWith("echo ")) {
                 System.out.println(input.substring(5));
             }
 
+            // type command
             else if (input.startsWith("type ")) {
                 String cmd = input.substring(5);
 
+                // builtins
                 if (cmd.equals("echo") ||
                     cmd.equals("exit") ||
                     cmd.equals("type")) {
                     System.out.println(cmd + " is a shell builtin");
-                } else {
+                    continue;
+                }
+
+                // search PATH
+                String pathEnv = System.getenv("PATH");
+                String[] paths = pathEnv.split(":");
+                boolean found = false;
+
+                for (String path : paths) {
+                    File file = new File(path, cmd);
+                    if (file.exists() && file.canExecute()) {
+                        System.out.println(cmd + " is " + file.getAbsolutePath());
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
                     System.out.println(cmd + ": not found");
                 }
             }
 
+            // unknown command
             else {
                 System.out.println(input + ": command not found");
             }
